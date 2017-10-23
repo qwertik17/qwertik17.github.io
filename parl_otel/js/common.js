6520381e619b39ba8e879a59d7639d6ea01fba9f
Vasiliy Pupkin <vpupkin@company.com>
2017-10-23 23:47:36 +0300

    commit

--- parl_otel/js/common.js
+++ parl_otel/js/common.js
@@ -1,16 +1,14 @@
-$(function() {
+jQuery(document).ready(function($) {
 
-	$('.date input').datepicker({
-        format: "dd.mm.yyyy",
-        startDate: "now()",
-        maxViewMode: 0,
-        todayBtn: "linked",
-		language: "ru",
-        keyboardNavigation: false,
-        autoclose: true,
-        todayHighlight: true,
-        orientation: "bottom auto"
-    });
+	$(".header-bg").height($(window).height());
+ 		
+	$(".date").flatpickr({
+		minDate: "today",
+		rangeSeparator: "по",
+		mode: "range",
+		dateFormat: "d.m.Y",
+		"locale": "ru"
+	});
 
     $(".phone-input").mask("+7(999) 999-9999");
 
@@ -26,6 +24,12 @@ $(function() {
 		$(".small-long-service").css({"height": slsh*2});
 	}
 	$(".y-video-more").css({"height": $(".y-video").height()});
+
+	if ($(window).width() < 768) {
+		$(".dropdown").click(function(event) {
+			$(this).children('ul').slideToggle(400);
+		});
+	}
 	$(window).resize(function(){
 		var ssh = $(".small-service").width();
 		var lsh = ssh * 2;
@@ -39,6 +43,7 @@ $(function() {
 			$(".small-long-service").css({"height": slsh*2});
 		}
 		$(".y-video-more").css({"height": $(".y-video").height()});
+		$(".header-bg").height($(window).height());
 	})
 
 
@@ -62,8 +67,60 @@ $(function() {
 
     $(".mobile-mnu").click(function(event) {
     	event.preventDefault();
-		$(".header").toggle(400);
+		$(".header").slideToggle(400);
     });
 
+    $('.zoom-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		closeOnContentClick: false,
+		closeBtnInside: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		image: {
+			verticalFit: true,
+			tError: 'Ошибка загрузки изображения'
+		},
+		gallery: {
+			enabled: true,
+			tPrev: '<i class="fa fa-long-arrow-left" aria-hidden="true"></i> Назад',
+			tNext: 'Далее <i class="fa fa-long-arrow-right" aria-hidden="true"></i>',
+			tCounter: '<span class="mfp-counter">%curr% из %total%</span>',
+			arrowMarkup: '<div class="gallery-arrow gallery-arrow-%dir%">%title%</div>'
+		},
+		zoom: {
+			enabled: true,
+			duration: 300, // don't foget to change the duration also in CSS
+			opener: function(element) {
+				return element.find('img');
+			}
+		}
+		
+	});
 
+	$(".up-btn").click(function(event) {
+		$("html").animate({scrollTop:0}, 500);
+		return false;
+	});
+
+	var $check_class = $('.fond-item .fond-item-content');
+	if ($check_class.length > 0) {
+		$('.fond-item .fond-item-content').equalHeights();
+	}	
+
+
+	$("#popup__toggle").on('click', function(event) {
+		event.preventDefault();
+		$(".modal-callback-wrap").animate({"opacity": "1"},200);
+		$(".modal-callback").slideDown(400);
+		$(".modal-callback-wrap").fadeIn(400);
+	});
+
+
+	$(".modal-callback-wrap,.close-modal-callback").click(function(event){
+		event.preventDefault();
+		$(".modal-callback-wrap").animate({"opacity": "0"},300);
+		$(".modal-callback").slideUp(400);
+	})
+
+	
 });
---
+++ parl_otel/libs/flatpickr/build.ts
@@ -0,0 +1,226 @@
+import * as fs from "fs";
+import { promisify } from "util";
+import { exec as execCommand } from "child_process";
+
+import * as glob from "glob";
+
+import * as uglifyJS from "uglify-js";
+
+import * as chokidar from "chokidar";
+import * as stylus from "stylus";
+import * as stylus_autoprefixer from "autoprefixer-stylus";
+
+import * as rollup from "rollup";
+import * as rollup_typescript from "rollup-plugin-typescript2";
+
+import * as path from "path";
+
+const pkg = require("./package.json");
+const version = `/* flatpickr v${pkg.version},, @license MIT */`;
+
+const paths = {
+  themes: "./src/style/themes/*.styl",
+  style: "./src/style/flatpickr.styl",
+  plugins: "./src/plugins",
+  l10n: "./src/l10n",
+};
+
+const rollupConfig = {
+  inputOptions: {
+    input: "",
+    plugins: [
+      (rollup_typescript as any)({
+        abortOnError: false,
+        cacheRoot: `/tmp/.rpt2_cache`,
+      }),
+    ],
+  },
+  outputOptions: {
+    file: "",
+    format: "umd",
+    banner: `/* flatpickr v${pkg.version}, @license MIT */`,
+  },
+};
+
+function logErr(e: Error | string) {
+  console.error(e);
+}
+
+const writeFileAsync = promisify(fs.writeFile);
+
+function startRollup(dev = false) {
+  return new Promise((resolve, reject) => {
+    execCommand(
+      `npm run rollup:${dev ? "start" : "build"}`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return reject();
+        }
+
+        console.log(stdout);
+        console.log(stderr);
+        resolve();
+      }
+    );
+  });
+}
+
+function resolveGlob(g: string) {
+  return new Promise<string[]>((resolve, reject) => {
+    glob(
+      g,
+      (err: Error | null, files: string[]) =>
+        err ? reject(err) : resolve(files)
+    );
+  });
+}
+
+async function readFileAsync(path: string) {
+  return new Promise<string>((resolve, reject) => {
+    fs.readFile(path, (err, buffer) => {
+      err ? reject(err) : resolve(buffer.toString());
+    });
+  });
+}
+
+function uglify(src: string) {
+  const minified = uglifyJS.minify(src, {
+    output: {
+      preamble: version,
+      comments: false,
+    },
+  } as any);
+
+  (minified as any).error && console.log((minified as any).error);
+  return minified.code;
+}
+
+async function buildScripts() {
+  try {
+    const transpiled = await readFileAsync("./dist/flatpickr.js");
+
+    writeFileAsync("./dist/flatpickr.min.js", uglify(transpiled)).catch(logErr);
+  } catch (e) {
+    logErr(e);
+  }
+}
+
+function buildExtras(folder: "plugins" | "l10n") {
+  return async function(changed_path?: string) {
+    const [src_paths, css_paths] = await Promise.all([
+      changed_path !== undefined
+        ? [changed_path]
+        : resolveGlob(`./src/${folder}/**/*.ts`),
+      resolveGlob(`./src/${folder}/**/*.css`),
+    ]);
+
+    await Promise.all([
+      ...src_paths.map(async sourcePath => {
+        const bundle = await rollup.rollup({
+          ...rollupConfig.inputOptions,
+          input: sourcePath,
+        } as any);
+        return bundle.write({
+          ...rollupConfig.outputOptions,
+          file: sourcePath.replace("src", "dist").replace(".ts", ".js"),
+          name: path.basename(sourcePath, path.extname(sourcePath)),
+        } as any);
+      }),
+      ...css_paths.map(async p => {
+        fs
+          .createReadStream(p)
+          .pipe(fs.createWriteStream(p.replace("src", "dist")));
+      }),
+    ]);
+
+    console.log("done.");
+  };
+}
+
+async function transpileStyle(src: string, compress = false) {
+  return new Promise<string>((resolve, reject) => {
+    stylus(src, {
+      compress,
+    } as any)
+      .include(`${__dirname}/src/style`)
+      .include(`${__dirname}/src/style/themes`)
+      .use(
+        stylus_autoprefixer({
+          browsers: pkg.browserslist,
+        })
+      )
+      .render(
+        (err: Error | undefined, css: string) =>
+          !err ? resolve(css) : reject(err)
+      );
+  });
+}
+
+async function buildStyle() {
+  const [src, src_ie] = await Promise.all([
+    readFileAsync(paths.style),
+    readFileAsync("./src/style/ie.styl"),
+  ]);
+
+  const [style, min, ie] = await Promise.all([
+    transpileStyle(src),
+    transpileStyle(src, true),
+    transpileStyle(src_ie),
+  ]);
+
+  writeFileAsync("./dist/flatpickr.css", style).catch(logErr);
+  writeFileAsync("./dist/flatpickr.min.css", min).catch(logErr);
+  writeFileAsync("./dist/ie.css", ie).catch(logErr);
+}
+
+const themeRegex = /themes\/(.+).styl/;
+async function buildThemes() {
+  const themePaths = await resolveGlob("./src/style/themes/*.styl");
+  themePaths.forEach(themePath => {
+    const match = themeRegex.exec(themePath);
+    if (!match) return;
+
+    readFileAsync(themePath)
+      .then(transpileStyle)
+      .then(css => writeFileAsync(`./dist/themes/${match[1]}.css`, css));
+  });
+}
+
+function setupWatchers() {
+  watch("./src/plugins", buildExtras("plugins"));
+  watch("./src/style/*.styl", () => {
+    buildStyle();
+    buildThemes();
+  });
+  watch("./src/style/themes", buildThemes);
+}
+
+function watch<F extends () => void>(path: string, cb: F) {
+  chokidar
+    .watch(path, {
+      awaitWriteFinish: {
+        stabilityThreshold: 100,
+      },
+    })
+    .on("change", cb)
+    .on("error", logErr);
+}
+
+function start() {
+  const devMode = process.argv.indexOf("--dev") > -1;
+  startRollup(devMode).then(buildScripts);
+
+  if (devMode) {
+    setupWatchers();
+  } else {
+    buildStyle();
+    buildThemes();
+    buildExtras("l10n")();
+    buildExtras("plugins")();
+  }
+}
+
+start();
+
+process.on("unhandledRejection", logErr);
