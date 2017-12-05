6701913756fb77c9db8e68d09648537d46737011
Vasiliy Pupkin <vpupkin@company.com>
2017-12-05 17:17:30 +0400

    commit

---
+++ oblaka/js/common.js
@@ -0,0 +1,60 @@
+jQuery(document).ready(function($) {
+	$('.insta .owl-carousel').owlCarousel({
+		loop:true,
+		margin:10,
+		nav:true,
+		autoplay: 3000,
+		autoplaySpeed: 500,
+		navSpeed: 500,
+		navText: [],
+		responsive:{
+			0:{
+				items:1
+			},
+			480:{
+				items:2
+			},
+			768:{
+				items:3
+			},
+			1000:{
+				items:4
+			},
+			1400:{
+				items:5
+			}
+		}
+	});
+
+	$('.partners .owl-carousel').owlCarousel({
+		loop:true,
+		margin:10,
+		nav:true,
+		autoplay: 3000,
+		autoplaySpeed: 500,
+		navSpeed: 500,
+		navText: [],
+    	autoWidth: true,
+		margin: 100,
+		center:true
+	});
+
+	if ( $(window).width()>600) {
+		$("#map").css({"height":$(".map-cover").outerHeight(true)+"px"});
+	};
+	$(window).resize(function(event) {
+		if ($(window).width()>600) {
+			$("#map").css({"height":$(".map-cover").outerHeight(true)+"px"});
+		};
+	});
+
+	$(".menu-btn").on('click', function(event) {
+		event.preventDefault();
+		$(".mobile-menu").slideDown(400);
+	});
+	$(".close-menu").on('click', function(event) {
+		event.preventDefault();
+		$(".mobile-menu").slideUp(400);
+	});
+
+})
