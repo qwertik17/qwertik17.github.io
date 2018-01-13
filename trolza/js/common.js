25739b483c623d8d0686a4a95d3c3062a142b928
Vasiliy Pupkin <vpupkin@company.com>
2018-01-13 14:53:54 +0400

    commit

--- trolza/js/common.js
+++ trolza/js/common.js
@@ -17,6 +17,15 @@ jQuery(document).ready(function($) {
 		swipe: 50,
 		interval: 10000
 	});
+
+	$(window).scroll(function(event) {
+		$(".submenu").animate({top: $(window).scrollTop()-20+"px"},70);
+	});
+
+	$("body").on('click', '.close-submenu', function(event) {
+		event.preventDefault();
+		$(".submenu").toggleClass('hide-submenu');
+	});
 			
 });
 
