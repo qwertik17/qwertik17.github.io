5f6785c0986f685593421853eb08f72b64aa9baa
Vasiliy Pupkin <vpupkin@company.com>
2018-01-15 16:30:17 +0400

    commit

--- trolza/js/common.js
+++ trolza/js/common.js
@@ -1,5 +1,12 @@
 jQuery(document).ready(function($) {
 
+	$('.advantages-carousel').owlCarousel({
+		loop:true,
+		margin:30,
+		nav:false,
+		responsive:{0:{items:1},480:{items:2},570:{items:3},768:{items:4},992:{items:5},1200:{items:6}}
+	})
+
 	$(".up-btn").hover(function() {
 		$(this).parent().addClass('top-up-hover');
 	}, function() {
@@ -14,19 +21,47 @@ jQuery(document).ready(function($) {
 	$(".phone-input").mask("+7(999) 999-9999");
 
 	$("#bus-carousel").carousel({
-		swipe: 50,
+		swipe: 20,
 		interval: 10000
 	});
+	$("#main-slider").carousel({
+		swipe: 20,
+		interval: 5000
+	});
+	$("#history-slider").carousel({
+		swipe: 20,
+		interval: 5000
+	});
 
-	$(window).scroll(function(event) {
-		$(".submenu").animate({top: $(window).scrollTop()-20+"px"},70);
+
+	$(window).bind('mousewheel DOMMouseScroll MozMousePixelScroll', function(event) {
+		delta = parseInt(event.originalEvent.wheelDelta || -event.originalEvent.detail);
+		if (delta >= 0) {
+			if ($(window).scrollTop()>$(".header").outerHeight()) {
+				$(".header").addClass('sticky-header');
+				$("body").css({"margin-top": $(".header").outerHeight()+'px'});
+			} else {
+				$(".header").removeClass('sticky-header');
+				$("body").css({"margin-top": '0px'});
+			}
+			
+		} else {
+			$(".header").removeClass('sticky-header');
+			$("body").css({"margin-top": '0px'});
+		}
 	});
 
 	$("body").on('click', '.close-submenu', function(event) {
 		event.preventDefault();
 		$(".submenu").toggleClass('hide-submenu');
 	});
-			
+
+
+	$(".hamburger-box, .site-overlay").click(function(event) {
+		$(".hamburger-box").toggleClass('hamburger--spin is-active');
+	});
+
+	
 });
 
 
